3fefb85c7bf43cc866619a1e70a0bf51958175ae
ryoutakimura <yellow11053@gmail.com>
2023-12-09 22:09:24 +0900

    postOutputを削除

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,20 +3,14 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
     @GetMapping("/input")
-    public String getInput() {
+    public String getInput(@RequestParam("previous") String previous,Model model){
+        model.addAttribute("previous",previous);
         return "input";
     }
-
-    @PostMapping("/output")
-    public String postOutput(@RequestParam("val") String val, Model model){
-        model.addAttribute("val",val);
-        return "output";
-            }
 
 }
