d52408fe255dadd77960054a733bc01738c6bc77
ryoutakimura <yellow11053@gmail.com>
2023-12-09 22:09:49 +0900

    postOutputメソッドを引っ越し

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -1,13 +1,17 @@
 package com.techacademy;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OutputController {
-    @GetMapping("/output")
-    public String postOutput() {
+    @PostMapping("/output")
+    public String postOutput(@RequestParam("val") String val, Model model){
+        model.addAttribute("val",val);
         return "output";
-    }
+            }
+
 
 }
